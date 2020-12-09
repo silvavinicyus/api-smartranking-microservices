3c8fafcd1cea5e51df48a2746b68139be47a3afd
Vinicyus <vinicyus346@gmail.com>
2020-12-09 17:51:07 -0300

    Primeiro endpoint 'criar-categoria'

--- micro-admin-backend/src/app.module.ts
+++ micro-admin-backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriaSchema } from './interfaces/categorias/categorias.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogadores.schema';
+require('dotenv').config()
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_CONNECTION,
+      {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false}
+    ),
+    MongooseModule.forFeature([
+      {name: 'Categoria', schema: CategoriaSchema},
+      {name: 'Jogador', schema: JogadorSchema}
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- micro-admin-backend/src/app.service.ts
+++ micro-admin-backend/src/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Jogador } from './interfaces/jogadores/jogadores.interface';
+import { Categoria } from './interfaces/categorias/categorias.interface';
+import { RpcException } from '@nestjs/microservices';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(
+    @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>,
+    @InjectModel('Jogador') private readonly jogadorModel: Model<Jogador>,
+  ){}
+
+  private readonly logger = new Logger(AppService.name);
+
+  async criarCategoria(categoria: Categoria): Promise<Categoria>{
+    
+    try{
+      const categoriaCriada = new this.categoriaModel(categoria);
+      return await categoriaCriada.save();
+    } catch(error){
+      this.logger.log(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
   }
+
 }
